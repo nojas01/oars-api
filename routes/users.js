981e308257a13ed777a7d01dfac41179477d5451
nojas01 <j.r.nede@gmail.com>
2017-12-21 11:04:21 +0100

    create new user from tablet route

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,18 @@ router.post("/login", function(req, res) {
   }
 
   models.User.findOne({ where: {username: req.body.username} }).then(user => {
-    if( !user ){
-      res.status(401).json({message:"no such user found"});
+    if( !user && req.body.token === 'tablet' ){
+      const newUser = req.body
+
+      models.User.create(newUser)
+        .then((users) => res.json({message:"user created"}))
+        .catch((error) => next(error))
     }
+
+    else if( !user ) {
+        res.status(401).json({message:"no such user found"});
+    }
+
     if(user.dataValues.password === req.body.password) {
       // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
       var payload = {id: user.dataValues.id};
