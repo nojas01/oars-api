ea404c4cca8a054ce8f6981e7e96164e8e80a302
nojas01 <j.r.nede@gmail.com>
2017-12-21 14:41:33 +0100

    created new file upload route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const trainings = require('./trainings')
 const users = require('./users')
 const rowers = require('./rowers')
 const ships = require('./ships')
+const fileuploads = require('./fileuploads')
 
 module.exports = {
   trainings,
   users,
   rowers,
-  ships
+  ships,
+  fileuploads
 }
