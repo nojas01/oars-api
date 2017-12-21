cbcc869aabcb59c87aaf05667d126aeb454ad14b
nojas01 <j.r.nede@gmail.com>
2017-12-21 11:06:58 +0100

    add comment to the user route

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.post("/login", function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
   }
-
+// Function to check if the user exists, or if its a new user coming from the tablet (in which case it will create a new user)
   models.User.findOne({ where: {username: req.body.username} }).then(user => {
     if( !user && req.body.token === 'tablet' ){
       const newUser = req.body
