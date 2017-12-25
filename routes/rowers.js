e7879eec98370a4acf755cad210d1aa8c1d7ff5f
nojas01 <j.r.nede@gmail.com>
2017-12-25 13:57:46 +0100

    edit code for rowers route to look and feel better. it is now more DRY

--- routes/rowers.js
+++ routes/rowers.js
@@ -1,21 +1,19 @@
-const models = require('../models');
-const express = require('express');
-const router = express.Router();
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.js')[env];
+const models    = require('../models')
+const express   = require('express')
+const router    = express.Router()
+const env       = process.env.NODE_ENV || 'development'
+const config    = require(__dirname + '/../config/config.js')[env]
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 router.get('/rowers', (req, res, next) => {
-  const Sequelize = require('sequelize');
-  const sequelize = new Sequelize(config.database, config.username, config.password, config)
   sequelize.query("SELECT * FROM `Rowers`", { type: Sequelize.QueryTypes.SELECT})
     .then((rowers) => res.json(rowers))
     .catch((error) => next(error))
-  });
+  })
 
 router.get('/rowers/:id', (req, res, next) => {
   const id = req.params.id
-  const Sequelize = require('sequelize');
-  const sequelize = new Sequelize(config.database, config.username, config.password, config)
   const question = "SELECT * FROM `Trainings` INNER JOIN `TrainingRower` ON TrainingRower.trainingid = Trainings.id INNER JOIN `Rowers` ON  TrainingRower.rowerid = Rowers.id WHERE Rowers.id ="
   const queryForSql = question + id
 
@@ -25,27 +23,23 @@ router.get('/rowers/:id', (req, res, next) => {
       res.json(rowers)
     })
      .catch((error) => next(error))
-  });
+  })
 
 router.post('/rowers', (req, res, next) => {
   const newRower = req.body
-  console.log(newRower)
+
   models.Rower.create(newRower)
     .then((rower) => res.json(rower))
     .catch((error) => next(error))
   })
 
 router.post('/rowersToTraining', (req, res, next) => {
-  const Sequelize = require('sequelize');
-  const sequelize = new Sequelize(config.database, config.username, config.password, config)
-  const TrainingId = req.body.TrainingId.toString(); //has to be a string, not a number!
-  const boat_number = req.body.boat_number.toString();//has to be a string, not a number!
+  const TrainingId = req.body.TrainingId.toString() //has to be a string, not a number!
+  const boat_number = req.body.boat_number.toString()//has to be a string, not a number!
   const array = req.body.RowerId
-  console.log(typeof array);
-  console.log(array);
 
   const newArray = array.map((id) => {
-    const RowerId = id.toString(); //has to be a string, not a number!
+    const RowerId = id.toString() //has to be a string, not a number!
     const values = "(" + RowerId + ", " + TrainingId + ", " + boat_number + ")"
     const question = "INSERT INTO `TrainingRower` (RowerId, TrainingId, boat_number) VALUES"
     const queryForDBSql = question + values
@@ -56,4 +50,4 @@ router.post('/rowersToTraining', (req, res, next) => {
   })
 })
 
-module.exports = router;
+module.exports = router
