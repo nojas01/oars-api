c64547112636c1ffc5f26d3e6d5a807cbba70537
nojas01 <j.r.nede@gmail.com>
2017-12-29 19:05:41 +0100

    add authentication to ships

--- routes/rowers.js
+++ routes/rowers.js
@@ -1,4 +1,5 @@
 const models    = require('../models')
+const passport  = require('../config/auth')
 const express   = require('express')
 const router    = express.Router()
 const env       = process.env.NODE_ENV || 'development'
@@ -6,14 +7,15 @@ const config    = require(__dirname + '/../config/config.js')[env]
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
-router.get('/rowers', (req, res, next) => {
+router.get('/rowers', passport.authorize('jwt', {session: false }), (req, res, next) => {
   sequelize.query("SELECT * FROM `Rowers`", { type: Sequelize.QueryTypes.SELECT})
     .then((rowers) => res.json(rowers))
     .catch((error) => next(error))
-  })
+})
 
-router.get('/rowers/:id', (req, res, next) => {
+router.get('/rowers/:id', passport.authorize('jwt', {session: false }), (req, res, next) => {
   const id = req.params.id
+  const account = req.account.id
   const question = "SELECT * FROM `Trainings` INNER JOIN `TrainingRower` ON TrainingRower.trainingid = Trainings.id INNER JOIN `Rowers` ON  TrainingRower.rowerid = Rowers.id WHERE Rowers.id ="
   const queryForSql = question + id
 
@@ -23,44 +25,44 @@ router.get('/rowers/:id', (req, res, next) => {
       res.json(rowers)
     })
      .catch((error) => next(error))
-  })
+})
 
-router.post('/rowers', (req, res, next) => {
+router.post('/rowers', passport.authorize('jwt', {session: false }), (req, res, next) => {
   const newRower = req.body
 
   models.Rower.create(newRower)
     .then((rower) => res.json(rower))
     .catch((error) => next(error))
-  })
+})
 
-  router.post('/rowersToTraining', (req, res, next) => {
-    const rowers = req.body.rowers
-    const trainingId = req.body.trainingId.toString() //has to be a string, not a number!
-    const boat_number = req.body.boat_number_name.toString()//has to be a string, not a number!
-    const shipId = req.body.shipId.toString()
+router.post('/rowersToTraining', passport.authorize('jwt', {session: false }), (req, res, next) => {
+  const rowers = req.body.rowers
+  const trainingId = req.body.trainingId.toString() //has to be a string, not a number!
+  const boat_number = req.body.boat_number_name.toString()//has to be a string, not a number!
+  const shipId = req.body.shipId.toString()
 
-    //loop over rowers
-    for (var i = 0; i < rowers.length; i++) {
-      const values = "(" + rowers[i] + ", " + trainingId + ", " + boat_number + ")"
-      const question = "INSERT INTO `TrainingRower` (RowerId, TrainingId, boat_number) VALUES "
-      const queryForDBSql = question + values
+  //loop over rowers
+  for (var i = 0; i < rowers.length; i++) {
+    const values = "(" + rowers[i] + ", " + trainingId + ", " + boat_number + ")"
+    const question = "INSERT INTO `TrainingRower` (RowerId, TrainingId, boat_number) VALUES "
+    const queryForDBSql = question + values
 
-      sequelize.query(queryForDBSql, { type: Sequelize.QueryTypes.UPDATE})
-        .then((rower) => res.json(rower))
-        .catch((error) => next(error))
-    }
-      //connect ship with training
-    const valueShip =  "(" + shipId + ", " + trainingId + ", " + boat_number + ")"
-    const questionShip = "INSERT INTO `TrainingShip` (ShipId, TrainingId, boat_number) VALUES "
-    const queryForDBSql2 = questionShip + valueShip
+    sequelize.query(queryForDBSql, { type: Sequelize.QueryTypes.UPDATE})
+      .then((rower) => res.json(rower))
+      .catch((error) => next(error))
+  }
+    //connect ship with training
+  const valueShip =  "(" + shipId + ", " + trainingId + ", " + boat_number + ")"
+  const questionShip = "INSERT INTO `TrainingShip` (ShipId, TrainingId, boat_number) VALUES "
+  const queryForDBSql2 = questionShip + valueShip
 
-    sequelize.query(queryForDBSql2, { type: Sequelize.QueryTypes.UPDATE})
-     .then((rower) => res.json(rower))
-     .catch((error) => next(error))
+  sequelize.query(queryForDBSql2, { type: Sequelize.QueryTypes.UPDATE})
+   .then((rower) => res.json(rower))
+   .catch((error) => next(error))
 
- }) ;
+})
 
-router.get('/rowersToTraining/:TrainingId/:boat_number', (req, res, next) => {
+router.get('/rowersToTraining/:TrainingId/:boat_number', passport.authorize('jwt', {session: false }), (req, res, next) => {
  const TrainingId = req.params.TrainingId;
  const boat_number = req.params.boat_number;
 
@@ -71,7 +73,8 @@ router.get('/rowersToTraining/:TrainingId/:boat_number', (req, res, next) => {
          attributes: ['TrainingId', 'RowerId', 'boat_number'],
          where: {
            TrainingId: +TrainingId,
-           boat_number: +boat_number
+           boat_number: +boat_number,
+
          }
        }
      }],
--- routes/ships.js
+++ routes/ships.js
@@ -1,21 +1,23 @@
-const models = require('../models');
-const express = require('express');
-const router = express.Router();
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.js')[env];
+const models    = require('../models');
+const passport  = require('../config/auth')
+const express   = require('express');
+const router    = express.Router();
+const env       = process.env.NODE_ENV || 'development';
+const config    = require(__dirname + '/../config/config.js')[env];
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
-router.get('/ships', (req, res, next) => {
+router.get('/ships', passport.authorize('jwt', {session: false }), (req, res, next) => {
   models.Ship.findAll()
     .then((ships) => res.json(ships))
     .catch((error) => next(error))
   });
 
-router.get('/ships/:id', (req, res, next) => {
+router.get('/ships/:id', passport.authorize('jwt', {session: false }), (req, res, next) => {
   const id = req.params.id
-  const Sequelize = require('sequelize');
-  const sequelize = new Sequelize(config.database, config.username, config.password, config)
+  const account = req.account.id
   const question = "SELECT * FROM `Trainings` INNER JOIN `TrainingShip` ON TrainingShip.trainingid = Trainings.id INNER JOIN `Ships` ON TrainingShip.shipid = Ships.id WHERE Ships.id ="
-  const queryForSql = question + id
+  const queryForSql = question + id + " AND UserId =" + account
 
   sequelize.query(queryForSql, { type: Sequelize.QueryTypes.SELECT})
     .then((ships) => {
@@ -25,7 +27,7 @@ router.get('/ships/:id', (req, res, next) => {
    .catch((error) => next(error))
   });
 
-  router.post('/ships', (req, res, next) => {
+  router.post('/ships', passport.authorize('jwt', {session: false }), (req, res, next) => {
     const newShip = req.body
 
     models.Ship.create(newShip)
