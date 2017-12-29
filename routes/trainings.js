302012df0de225875743b680a02cea5064831534
nojas01 <j.r.nede@gmail.com>
2017-12-29 16:42:42 +0100

    token now needed for the creation ofd a training

--- routes/trainings.js
+++ routes/trainings.js
@@ -5,30 +5,26 @@ const passport = require('../config/auth')
 
 router
   .get('/trainings', passport.authorize('jwt', {session: false }), function(req, res) {
-    models.Training.findAll({
-      include:[{
-        model: models.User,
-        where: {
-          id: req.account.id
-        }
-      }]
-    }).then(function(trainings) {
+    models.Training.findAll({ where: { UserId: req.account.id }})
+    .then(function(trainings) {
       res.json(trainings)
       })
   })
 
-  .get('/trainings/:id', (req, res, next) => {
+  .get('/trainings/:id', passport.authorize('jwt', {session: false }), (req, res, next) => {
     const id = req.params.id
     models.Training.findById(id)
       .then((training) => {
-        if (!training) { return next() }
+        if (!training || training.UserId != req.account.id) { return next() }
         res.json(training)
       })
       .catch((error) => next(error))
   })
 
-  .post('/trainings', (req, res, next) => {
+  .post('/trainings', passport.authorize('jwt', {session: false }), (req, res, next) => {
       const newTraining = req.body
+      newTraining.UserId = req.account.id
+      
       models.Training.create(newTraining)
         .then((training) => {
           res.json(training)
