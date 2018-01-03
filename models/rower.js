e6e109acb68b8f47024f7693b109c55147e792d5
nojas01 <j.r.nede@gmail.com>
2018-01-03 14:31:11 +0100

    add rower to user

--- models/rower.js
+++ models/rower.js
@@ -1,8 +1,8 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Rower = sequelize.define('Rower', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
@@ -11,7 +11,12 @@ module.exports = (sequelize, DataTypes) => {
   })
   Rower.associate = function(models) {
     Rower.belongsToMany(models.Training, { through: 'TrainingRower' })
-
+    Rower.belongsTo(models.User, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    })
   }
   return Rower
 }
